3a167a6ed98419ebc37cbf63395bbd5afe9103a0
wrufesh singh <wrufesh@gmail.com>
2017-06-19 17:32:32 +0545

    local settings added in respect of database setting and cache backend

--- newsproject/settings/dev.py
+++ newsproject/settings/dev.py
@@ -23,3 +23,8 @@ INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ newsproject/settings/local_sample.py
@@ -0,0 +1,24 @@
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
