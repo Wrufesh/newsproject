d22d1e88def4bdf17a6dfc8f66755ec1c837f805
wrufesh singh <wrufesh@gmail.com>
2017-06-19 18:13:23 +0545

    template being added

--- news/views.py
+++ news/views.py
@@ -7,6 +7,15 @@ from news.models import News
 class NewsListView(ListView):
     model = News
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.request.GET.get('category_id', None)
+        if category_id:
+            context['object_list']  = News.objects.filter(category__id=category_id).order_by('publication_date')
+        else:
+            context['object_list'] = News.objects.all().order_by('publication_date')
+        return context
+
 
 class NewsDetailView(DetailView):
     model = News
