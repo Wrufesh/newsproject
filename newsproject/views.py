71d3ed4539c703c146e8aa31ff8c5c12ee95d89c
wrufesh singh <wrufesh@gmail.com>
2017-06-21 17:48:22 +0545

    all news by category passed to index context

--- newsproject/views.py
+++ newsproject/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
-from news.models import News
+from news.models import News, Category
 
 
 def index(request):
     carousel_news = News.objects.all()
     context = dict()
     context['carousel_news'] = carousel_news
+
+    news_by_category = []
+
+    for category in Category.menus:
+        news_by_category.append((category.name, News.objects.filter(category=category)))
+
+    context['news_by_category'] = news_by_category
+
     return render(request, 'index.html', context)
