8d65371d87a20ddea2581ccb43d2989a015ec969
wrufesh singh <wrufesh@gmail.com>
2017-06-27 13:24:16 +0545

    custom header done

--- news/templatetags/category.py
+++ news/templatetags/category.py
@@ -1,4 +1,6 @@
 from django import template
+from django.conf import settings
+from django.contrib.sites.models import Site
 
 from news.models import Category, News, Tag
 
@@ -14,6 +16,12 @@ def get_current_categories():
 def get_latest_news():
     return News.objects.all().order_by('published_date')[:10]
 
+
 @register.simple_tag
 def get_all_tags():
     return Tag.objects.all()
+
+
+@register.simple_tag
+def get_site_object():
+    return Site.objects.get(id=settings.SITE_ID)
