e3b61a1a29e9b49b7caa70dee938e8190afb5616
wrufesh singh <wrufesh@gmail.com>
2017-09-07 17:08:47 +0545

    static and media root set

--- newsproject/settings/base.py
+++ newsproject/settings/base.py
@@ -108,8 +108,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', '..', 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', '..', 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
 
 SITE_ID = 1
